756b254a9e0209597721aadc26116fe33fde8800
Rafael Echart <rafaelechartsaltos@gmail.com>
2021-12-01 11:04:51 -0500

    Added Router for 2 pages

--- src/components/ToDoContainer.js
+++ src/components/ToDoContainer.js
@@ -1,22 +1,21 @@
 import React, { useState, useEffect } from "react";
-import {Route, Switch} from 'react-router-dom'
 import { v4 as uuidv4 } from "uuid";
 import Header from "./Header";
 import InputToDo from "./InputToDo";
 import ToDoList from "./ToDoList";
 
+
 const ToDoContainer = () => {
   const [todoList, setTodoList] = useState([]);
 
   useEffect(() => {
-    const localStorageMemory = JSON.parse(localStorage.getItem('dataTasks'))
+    const localStorageMemory = JSON.parse(localStorage.getItem("dataTasks"));
 
-    if(localStorageMemory && localStorageMemory.length){
-      setTodoList(localStorageMemory)
+    if (localStorageMemory && localStorageMemory.length) {
+      setTodoList(localStorageMemory);
     } else {
-      setTodoList([])
+      setTodoList([]);
     }
-   
   }, []);
 
   const handleChange = (id) => {
@@ -31,36 +30,34 @@ const ToDoContainer = () => {
         return toDoItem;
       });
 
-      localStorage.setItem('dataTasks', JSON.stringify(newState))
+      localStorage.setItem("dataTasks", JSON.stringify(newState));
       return newState;
     });
   };
 
-  const deleteToDo = id => {
-    const newTaskList = todoList.filter(toDoItem => toDoItem.id !== id)
-    setTodoList(newTaskList)
-    localStorage.setItem('dataTasks', JSON.stringify(newTaskList))
-
+  const deleteToDo = (id) => {
+    const newTaskList = todoList.filter((toDoItem) => toDoItem.id !== id);
+    setTodoList(newTaskList);
+    localStorage.setItem("dataTasks", JSON.stringify(newTaskList));
   };
 
-  const addTodoItem = title => {
+  const addTodoItem = (title) => {
     const newToDo = {
       id: uuidv4(),
       title: title,
-      completed: false
-    }
-    setTodoList(prevState => [...prevState, newToDo])
+      completed: false,
+    };
+    setTodoList((prevState) => [...prevState, newToDo]);
 
-    const localStorageMemory = JSON.parse(localStorage.getItem('dataTasks'))
+    const localStorageMemory = JSON.parse(localStorage.getItem("dataTasks"));
 
-    if(localStorageMemory && localStorageMemory.length){
-      localStorageMemory.push(newToDo)
-      localStorage.setItem('dataTasks', JSON.stringify(localStorageMemory))
+    if (localStorageMemory && localStorageMemory.length) {
+      localStorageMemory.push(newToDo);
+      localStorage.setItem("dataTasks", JSON.stringify(localStorageMemory));
     } else {
-      let tempNewMemory = [newToDo]
-      localStorage.setItem('dataTasks', JSON.stringify(tempNewMemory))
+      let tempNewMemory = [newToDo];
+      localStorage.setItem("dataTasks", JSON.stringify(tempNewMemory));
     }
-    
   };
 
   const setUpdate = (updatedTitle, id) => {
@@ -69,28 +66,32 @@ const ToDoContainer = () => {
         if (toDoItem.id === id) {
           return {
             ...toDoItem,
-            title: updatedTitle
+            title: updatedTitle,
           };
         }
         return toDoItem;
       });
 
-      localStorage.setItem('dataTasks', JSON.stringify(newState))
+      localStorage.setItem("dataTasks", JSON.stringify(newState));
       return newState;
     });
-  }
+  };
 
   return (
     
-
-    <div className="container" >
-      <div className="inner">
-      <Header/>
-      <InputToDo addTodoProps={addTodoItem}/>
-      <ToDoList data={todoList} handleChangeProps={handleChange} deleteTodoProps={deleteToDo} setUpdateProps={setUpdate} />
-      </div>
-    </div>
-  
+        <div className="container">
+          <div className="inner">
+            <Header />
+            <InputToDo addTodoProps={addTodoItem} />
+            <ToDoList
+              data={todoList}
+              handleChangeProps={handleChange}
+              deleteTodoProps={deleteToDo}
+              setUpdateProps={setUpdate}
+            />
+          </div>
+        </div>
+     
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
-import ToDoContainer from './components/ToDoContainer';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import ToDoContainer from "./components/ToDoContainer";
+import reportWebVitals from "./reportWebVitals";
+import About from "./pages/About";
+import NotMatch from "./pages/NotMatch";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-    <ToDoContainer />
+      <Routes>
+        <Route exact path="/" element={<ToDoContainer />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotMatch />} />
+      </Routes>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 reportWebVitals();
