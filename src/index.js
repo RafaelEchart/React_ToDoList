e22f52c9e9f2b5eab9d2eb3b118d3500796dbe95
Rafael Echart <rafaelechartsaltos@gmail.com>
2021-12-01 12:04:53 -0500

    React router new pages and nested routes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import ToDoContainer from "./components/ToDoContainer";
 import reportWebVitals from "./reportWebVitals";
 import About from "./pages/About";
 import NotMatch from "./pages/NotMatch";
+import Navbar from "./components/Navbar"
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Routes>
-        <Route exact path="/" element={<ToDoContainer />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotMatch />} />
-      </Routes>
+    <Navbar/>
+      <Switch>
+        <Route exact path="/" component={ToDoContainer} />
+        <Route path="/about" component={About} />
+        <Route path="*" component={NotMatch} />
+      </Switch>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,25 @@
-const About = () => {
-  return <div>hello from about page</div>
+import { Route, Link, useRouteMatch } from 'react-router-dom';
+import SinglePage from "./SinglePage"
+
+
+const About = (props) => {
+  const { url, path } = useRouteMatch()
+  
+
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to={`${url}/about-app`}>About App</Link>
+        </li>
+        <li>
+          <Link to={`${url}/about-author`}>About Author</Link>
+        </li>
+      </ul>
+      <Route path={`${path}/:section`}>
+        <SinglePage />
+      </Route>
+    </div>
+  )
 }
 export default About
